cba3e92bfb5c3088f582d4791584a3b05ad92b6f
Konstantin Vlasov <support@flint-inc.ru>
2018-01-06 21:16:21 +0300

    Added stub source files

---
+++ StartupHandler/main.cpp
@@ -0,0 +1,13 @@
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+#include <Windows.h>
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hInstance);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(nCmdShow);
+
+	return 0;
+}
